40d036f0f1996189a41bf301583c647bcbcd75a4
kipe <tkrs23280810@gmail.com>
2020-03-20 11:57:56 +0900

    finish chapter6-3

--- shop/shop_cartlook.php
+++ shop/shop_cartlook.php
@@ -26,18 +26,50 @@ else{
 
 	try{
 		$cart = $_SESSION['cart'];
+		$max = count($cart);
 
 		$dsn = 'mysql:dbname=product;host=localhost;charset=utf8';
 		$user = 'root';
 		$password = 'root';
 		$dbh = new PDO($dsn,$user,$password);
 		$dbh -> setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+		foreach ($cart as $key => $val) {
+			$sql = 'SELECT code,name,price,gazou FROM mst_product WHERE code=?';
+			$stmt = $dbh -> prepare($sql);
+			$data[0] = $val;
+			$stmt -> execute($data);
+
+			$rec = $stmt -> fetch(PDO::FETCH_ASSOC);
+
+			$pro_name[] = $rec['name'];
+			$pro_price[] = $rec['price'];
+			if ($rec['gazou'] == '') {
+				$pro_gazou[] = '';
+			}
+			else{
+				$pro_gazou[] = '<img src="../product/gazou/'.$rec['gazou'].'">';
+			}
+		}
+		$dbh = null;
+
 	}
 	catch(Exception $e) {
     print '只今、障害により大変ご迷惑お掛けしております。';
     exit();
 	}
 	?>
+
+	カートの中身<br>
+	<br>
+	<?php for ($i=0; $i < $max; $i++) {
+	?>
+
+    <?php print $pro_name[$i]; ?>
+    <?php print $pro_gazou[$i]; ?>
+    <?php print $pro_price[$i]; ?>円
+    <br>
+  <?php } ?>
 
 	<form>
 	<input type="button" onclick="history.back()" value="戻る">
