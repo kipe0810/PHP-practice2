77088eb307136f5a8ce983250796208699f6c83f
kipe <tkrs23280810@gmail.com>
2020-04-03 15:48:24 +0900

    chapter7-3

--- shop/shop_form_done.php
+++ shop/shop_form_done.php
@@ -1,3 +1,8 @@
+<?php
+  session_start();
+  session_regenerate_id(true);
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,25 +12,30 @@
 <body>
 
 	<?php
+	  try{
+	  	require_once('../common/common.php');
 
-	require_once('../common/common.php');
+			$post = sanitize($_POST);
 
-	$post = sanitize($_POST);
+			$onamae = $post['onamae'];
+			$email = $post['email'];
+			$postal1 = $post['postal1'];
+			$postal2 = $post['postal2'];
+			$address = $post['address'];
+			$tel = $post['tel'];
 
-	$onamae = $post['onamae'];
-	$email = $post['email'];
-	$postal1 = $post['postal1'];
-	$postal2 = $post['postal2'];
-	$address = $post['address'];
-	$tel = $post['tel'];
-
-	print $onamae.'様<br>';
-  print 'ご注文ありがとうございました。<br>';
-  print $email.'にメールを送りましたのでご確認ください。<br>';
-  print '商品は以下の住所に発送させていただきます。<br>';
-  print $postal1.'-'.$postal2.'<br>';
-  print $address.'<br>';
-  print $tel.'<br>';
+			print $onamae.'様<br>';
+		  print 'ご注文ありがとうございました。<br>';
+		  print $email.'にメールを送りましたのでご確認ください。<br>';
+		  print '商品は以下の住所に発送させていただきます。<br>';
+		  print $postal1.'-'.$postal2.'<br>';
+		  print $address.'<br>';
+		  print $tel.'<br>';
+	  }
+	  catch(Exception $e){
+	  	print '只今、障害により大変ご迷惑をお掛けしております。';
+	  	exit();
+	  }
 
 	?>
 
